efd213815a82a50db2ee5850a803107dd3562bb7
khlorghaal <khlorghaal@live.com>
2018-12-26 19:22:31 -0500

    converted to monolith comp unit

---
+++ doot.cpp
@@ -0,0 +1,23 @@
+//#include ".impl.hpp"
+
+#include "bytebuf.impl.hpp"
+#include "console.impl.hpp"
+#include "file.impl.hpp"
+#include "gvars.impl.hpp"
+#include "os_posix.impl.hpp"
+#include "os_win.impl.hpp"
+#include "string.impl.hpp"
+#include "tests.impl.hpp"
+#include "timer.impl.hpp"
+#include "trans2d.impl.hpp"
+#include "weave.impl.hpp"
+
+
+
+
+
+
+
+
+
+
